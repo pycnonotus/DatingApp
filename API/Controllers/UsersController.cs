304c197c17ac04db5ba6d15309ed44e02beb88dc
c1c <c1c@LAPTOP-RKSNG5P6>
2020-10-14 23:43:20 +0300

    added the user api

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [ApiController]
+    [Route ("/api/[controller]")]
+    public class UsersController : ControllerBase {
+        private readonly DataContext context;
+        public UsersController (DataContext context) {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUsers>> GetUsers () {
+            var users = context.Users.ToList ();
+            return users;
+        }
+
+        /// <summary>
+        /// Get a  user by an id
+        /// </summary>
+        [HttpGet ("{id}")]
+        public ActionResult<AppUsers> GetUser (int id) {
+            return context.Users.Find (id);
+        }
+
+    }
+}
